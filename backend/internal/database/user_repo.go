93cc3fdb6c7938bda2028cad695bfcf0a397b6ac
Denis <nelkner.denis@web.de>
2025-06-21 17:31:01 +0200

    starting

--- backend/internal/database/user_repo.go
+++ backend/internal/database/user_repo.go
@@ -17,3 +17,19 @@ type User struct {
 type UserRepository struct {
     DB *sql.DB
 }
+func (r *UserRepository) FindByEmail(email string) (*User, error) {
+    query := `SELECT user_id, email, password_hash, role FROM users WHERE email = $1`
+
+    var user User
+    row := r.DB.QueryRow(query, email)
+    err := row.Scan(&user.UserID, &user.Email, &user.PasswordHash, &user.Role)
+
+    if err != nil {
+        if errors.Is(err, sql.ErrNoRows) {
+            return nil, nil // Kein User gefunden
+        }
+        return nil, err // Technischer Fehler
+    }
+
+    return &user, nil
+}
